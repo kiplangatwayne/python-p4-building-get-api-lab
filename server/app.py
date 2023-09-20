18e04963d3965e24b018696f96ecb1e513e6e07c
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-09-20 14:51:47 +0300

    completed Assingment

--- server/app.py
+++ server/app.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
 
 from flask import Flask, make_response, jsonify
-from flask_migrate import Migrate
-
-from models import db, Bakery, BakedGood
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
+db = SQLAlchemy(app)
 
-db.init_app(app)
+class Bakery(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+
+    def to_dict(self):
+        return {'id': self.id, 'name': self.name}
+
+class BakedGood(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakery.id'), nullable=False)
+    bakery = db.relationship('Bakery', backref=db.backref('baked_goods', lazy=True))
+
+    def to_dict(self):
+        return {'id': self.id, 'name': self.name, 'price': self.price, 'bakery_id': self.bakery_id}
 
 @app.route('/')
 def index():
@@ -20,19 +32,45 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
+
+    response = make_response(jsonify(bakeries_serialized), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if bakery:
+        bakery_serialized = bakery.to_dict()
+        response = make_response(jsonify(bakery_serialized), 200)
+    else:
+        response = make_response(jsonify({'error': 'Bakery not found'}), 404)
+
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price).all()
+    baked_goods_by_price_serialized = [bg.to_dict() for bg in baked_goods_by_price]
+
+    response = make_response(jsonify(baked_goods_by_price_serialized), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive:
+        most_expensive_serialized = most_expensive.to_dict()
+        response = make_response(jsonify(most_expensive_serialized), 200)
+    else:
+        response = make_response(jsonify({'error': 'No baked goods found'}), 404)
+
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
